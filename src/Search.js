2e92d4c7720a89cc9d5bab153f61352bcf711fb1
Ariana <ariana.berger@gmail.com>
2019-08-19 19:12:51 -0400

    Update search to class component with state

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Component} from 'react';
+import { Form, Button } from 'react-bootstrap';
 
-const Search = () => {
-  return (
-    <div>
-      Search box goes here
-    </div>
-  )
+class Search extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ''
+    }
+  }
+
+  handleOnChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="searchInput">
+            <Form.Label>Search:</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              name="search"
+              placeholder="Search"
+              value={this.state.search}
+              onChange={this.handleOnChange}
+             />
+          </Form.Group>
+          <div>
+            <Button variant="info" type="submit" block>
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </div>
+    )
+  }
 }
 
 export default Search;
