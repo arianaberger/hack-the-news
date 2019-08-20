e2f929855d1128d9b4753b284bacdcc9caeece8d
Ariana <ariana.berger@gmail.com>
2019-08-20 12:43:40 -0400

    Properly format results and format date using react moment

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 const Results = (props) => {
 
@@ -9,9 +10,9 @@ const Results = (props) => {
     let date = result.created_at
 
     return (
-      `<li>
-        ${author} - <a href="${url}" target=_blank>${title}</a> - ${date}
-      </li>`
+      <li>
+        {author} - <a href={url} target='_blank'>{title}</a> - <Moment format="MMM DD, YYYY">{date}</Moment>
+      </li>
     )
   })
 
