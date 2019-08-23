0a17e727ca79289c527e669ebf11e9403f3bd58a
Ariana <ariana.berger@gmail.com>
2019-08-22 20:36:26 -0400

    Remove files and dependencies because app stopped working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Search from './Search';
 import logo from './logo.svg';
 
 
-class App extends Component {
+function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
